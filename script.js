bb15f12e0e94af2606e4c241af79ada0242d7a43
Gustavo Carneiro <gustavocarneiro.a@gmail.com>
2021-12-10 11:26:05 -0300

    [update] Mobile Responsive

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ window.addEventListener("load", function (event) {
             },
             attributes: [],
             imgUrl: 'https://picsum.photos/120/120',
-            characterName: 'Lorem Ipsum',
-            summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempor vulputate velit, eu convallis leo eleifend vel. Ut ullamcorper lacinia accumsan.',
-            quote: `Nulla sit amet nunc in ligula finibus facilisis. Donec vitae nulla pharetra, bibendum nisl non, pellentesque arcu. Aenean venenatis commodo diam, placerat dignissim libero bibendum ut. Donec efficitur id turpis eu pretium. Cras volutpat pulvinar venenatis. Curabitur viverra erat sit amet eros porttitor mattis. Donec venenatis semper urna vel egestas. Pellentesque id lectus lacus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed ac pulvinar odio, at accumsan odio.`
+            characterName: 'Nome de teste',
+            summary: 'Altere tudo nas configurações: link da foto, este sumário e até a descrição abaixo.',
+            quote: `Projeto em teste, feito para o grupo Patch Note do Mundo. Qualquer alteração pode solicitar diretamente para mim <a href="https://www.facebook.com/gustavo.almeida.589583/?show_switched_toast=0&show_invite_to_follow=0&show_switched_tooltip=0&show_podcast_settings=0&show_community_transition=0&show_community_review_changes=0"> usando este link aqui ;) </a>. Sabendo ou não sobre programação Web, estou aberto a críticas e sugestões.`
         },
 
         methods: {
